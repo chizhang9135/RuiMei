b16a2816dc91158a74fc80b316a0d9307787cfbb
Chi Zhang <chizhang9135@icloud.com>
2023-07-10 22:35:46 +0800

    test

--- school-item.js
+++ school-item.js
@@ -5,53 +5,53 @@ class SchoolItem extends HTMLElement {
     }
 
     connectedCallback() {
-        const data = JSON.parse(this.getAttribute('data'));
-
-        this.shadowRoot.innerHTML = `
+      const data = JSON.parse(this.getAttribute('data'));
+  
+      this.shadowRoot.innerHTML = `
       <style>
-        .school-card {
+          .school-card {
           width: 300px;
           border: 1px solid #ccc;
           border-radius: 5px;
           padding: 20px;
           background-color: #ffffff;
           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-        .school-image {
+          }
+          .school-image {
           width: 100%;
           height: auto;
-        }
-        .school-info {
+          }
+          .school-info {
           margin-top: 10px;
-        }
-        .school-info > .school-info > * {
+          }
+          .school-info > .school-info > * {
           margin: 5px 0;
-        }
-        
-        .school-card:hover {
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
-        
-        a div {
-            color: black;
-        }
-        
-        
-        
+          }
+          
+          .school-card:hover {
+              box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+          }
+          
+          a div {
+              color: black;
+          }
+          
       </style>
       <a href="${data.redirectLink}" style="text-decoration:none">
       <div class="school-card">
-        <img class="school-image" src="${data.picture}" alt="${data.name}">
-        <div class="school-info">
-          <h3>${data.name}</h3>
-          <p>${data.address}</p>
-          <p><strong>Type:</strong> ${data.type}</p>
-          <p><strong>Level:</strong> ${data.level}</p>
-        </div>
+          <img class="school-image" src="${data.picture}" alt="${data.name}">
+          <div class="school-info">
+          <h3>${data.name_cn}</h3>
+          <h3>${data.name_en}</h3>
+          <p><strong>地址:</strong> ${data.address}</p>
+          <p><strong>大致花费 (美金):</strong> ${data.cost}</p>
+          </div>
       </div>
       </a>
-    `;
-    }
+      `;
+  }
+  
+  
 }
 
 customElements.define('school-item', SchoolItem);
